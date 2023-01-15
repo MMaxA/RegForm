f99d402ebe52472498f5e4bb4e1e1e55acddc7c0
Медведев Максим Александрович <medvedevma@rambler.ru>
2023-01-16 02:57:56 +0500

    homework-01

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
-'use strict'
+"use strict";
 
-console.log("Hello");
+let user = "Maxim";
+console.log("Hello, " + user);
+
+//-----Homework-01--------
+let money = 3000;
+let profit = "веб-разработка";
+let expenses = "Питание, бензин, ЖКХ, доп. расходы";
+let purpose = 5000;
+let period = 6;
+let budgetDay = money / 30;
+
+//-----Output----------
+console.log(typeof money);
+console.log(typeof profit);
+console.log(expenses.length);
+console.log("----------------------");
+console.log(`Период равен ${period} месяцев. Цель - заработать ${purpose} долларов`);
+console.log(`Дневной бюджет равен ${budgetDay}`);

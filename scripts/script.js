58400c17ab52566615242479372ece871bb05fbe
Медведев Максим Александрович <medvedevma@rambler.ru>
2023-01-19 23:36:27 +0500

    homework-02

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,51 @@
 "use strict";
 
-let user = "Maxim";
+console.log("------- Homework-02 ----------");
+let user = prompt("Как Вас зовут?");
 console.log("Hello, " + user);
 
-//-----Homework-01--------
-let money = 3000;
-let profit = "веб-разработка";
-let expenses = "Питание, бензин, ЖКХ, доп. расходы";
-let purpose = 5000;
-let period = 6;
-let budgetDay = money / 30;
+//-----InputData--------
+let money = prompt("Ваш месячный доход?", 0);
+let expenses = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую",
+  "Вода, бензин"
+);
+let amount = prompt(
+  "Во сколько обойдуться обязательные статьи расходов?",
+  3000
+);
+let deposit = confirm("Есть ли у Вас счет в банке?");
+let purpose = prompt("Сколько средств Вы бы хотели накопить?");
+
+let budgetMonth = money - amount;
+let period = Math.ceil(purpose / budgetMonth);
+let budgetDay = Math.floor(budgetMonth / 30);
 
 //-----Output----------
-console.log(typeof money);
-console.log(typeof profit);
-console.log(expenses.length);
-console.log("----------------------");
-console.log(`Период равен ${period} месяцев. Цель - заработать ${purpose} долларов`);
-console.log(`Дневной бюджет равен ${budgetDay}`);
+console.log(`Финансовая цель - заработать ${purpose} рублей`);
+console.log(`Бюджет на месяц - ${budgetMonth}`);
+console.log(`Обязательные расходы: ${amount}`);
+console.log(`Месяцев до достижения финансовой цели: ${period}`);
+console.log(`Дневной бюджет, рублей: ${budgetDay}`);
 
-//-----Homework-01_Hard-------------
-let newExpenses = expenses.toLowerCase();
-let expensesArray = [];
-expensesArray = newExpenses.split(", ");
-console.log(expensesArray);
+console.log("--------Через if..else if-----------");
+if (budgetDay > 60000) {
+  console.log("У Вас высокий уровень дохода");
+} else if (30000 < budgetDay <= 60000) {
+  console.log("У Вас средний уровень дохода");
+} else if (0 < budgetDay <= 30000) {
+  console.log("К сожалению, Ваш уровень дохода ниже среднего");
+} else if (budgetDay <= 0) {
+  console.log("Упс, что-то пошло не так...");
+} else {
+  console.log("Значения вне интервала оценивания");
+}
+
+console.log("------Через тернарный оператор------");
+console.log(
+    budgetDay > 60000 ? "У Вас высокий уровень дохода" : 
+    30000 < budgetDay <= 60000 ? "У Вас средний уровень дохода" : 
+    0 < budgetDay <= 30000 ? "К сожалению, Ваш уровень дохода ниже среднего" : 
+    budgetDay < 0 ? "Упс, что-то пошло не так..." : 
+    "Значения вне интервала оценивания"
+);

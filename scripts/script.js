ac4f73f21aeae5583ea2830fad0d4326d6210672
Медведев Максим Александрович <medvedevma@rambler.ru>
2023-02-13 12:48:39 +0500

    homework-06

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,56 @@
 "use strict";
 
-// console.log("------- Homework-05 ----------");
-// В задаче не совсем понятно определяется "Бюджет".
-// Судя по всему, мы имеем дело со свободными средствами, которые остались после учета доп.доходов и расходов.
-// Это не совсем бюджет, это - оставшиеся средства, прибыль или сбережения. Комментарии в консоль выводил исходя из этой логики.
+console.log("------- Homework-06 ----------");
+// Получение данных с сервера
 
-let money = prompt("Ваш месячный доход?", 0);
-while(isNaN(money)) {
-  money = prompt("Необходимо ввести число! Ваш месячный доход?", 0);
+const url = 'https://reqres.in/api/users?per_page=12';
+
+async function getData() {
+    try {
+        console.log('-----------');
+        console.log('Пункт 1: Получение данных');
+        const response = await fetch(url);
+        const data = await response.json();
+        let userList = {};
+        userList = data.data;
+        console.log(userList);
+
+        console.log('-----------');
+        console.log('Пункт 2: Фамилии пользователей');
+
+        userList.forEach((item, index, array) => {
+            console.log(item.last_name);
+        })
+
+        console.log('-----------');
+        console.log('Пункт 3: Все фамилии на F');
+
+        userList.forEach((item, index, array) => {
+            if(item.last_name[0] === "F") {
+                console.log(item.last_name);
+            }
+        });
+
+        console.log('-----------');
+        console.log('Пункт 4:');
+        const newUserList = userList.reduce((acc, user) => {
+            acc.push(`${user.first_name} ${user.last_name}`);
+            return acc;
+        }, []);
+
+        console.log(`Наша база содержит данные следующих пользователей: ${newUserList.join(", ")}.`);
+
+        console.log('-----------');
+        console.log('Пункт 5: Вывод всех ключей объекта');
+
+        Object.keys(userList[0]).forEach((key) => {
+            console.log(key);
+        });
+
+        return data;
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-let profit = prompt("Введите планируемые статьи доп. доходов:", "Веб-разработка, депозит в банке, фриланс");
-let extraMoney = prompt(`Введите сумму доходов за ваши доп.работы: ${profit}`, 0);
-while(isNaN(extraMoney)) {
-  extraMoney = prompt(`Необходимо ввести число! Введите сумму доходов за ваши доп.работы: ${profit}`, 0);
-}
-
-let expenses = prompt("Перечислите обязательные расходы за рассчитываемый период (через запятую)", "Вода, бензин");
-let amount = prompt(`Во сколько обойдутся обязательные статьи расходов: ${expenses}?`, 0);
-while(isNaN(amount)) {
-  amount = prompt(`Необходимо ввести число! Во сколько обойдутся обязательные статьи расходов: ${expenses}?`, 0);
-}
-
-let purpose = prompt("Сколько средств Вы бы хотели накопить?");
-while(isNaN(purpose)) {
-  purpose = prompt("Необходимо ввести число! Сколько средств Вы бы хотели накопить?");
-}
-
-//функция расчета свободных средств
-function getAccumulatedIncome(money, extraMoney, amount) {
-  return (Number(money) + Number(extraMoney)) - Number(amount);
-}
-
-//функция расчета количества месяцев до достижения цели
-function getTargetMonth(income) {
-    if(income > 0){
-      return `Цель будет достигнута через ${Math.ceil(purpose / income)} месяца`;
-    } else return "Цель не будет достигнута!";
-}
-
-//получаем все необходимые переменные для вывода в консоль
-const accumulatedIncome = getAccumulatedIncome(money, extraMoney, amount);
-const targetMonth = getTargetMonth(accumulatedIncome);
-const budgetDay = Math.floor(accumulatedIncome / 30);
-
-//-----Output----------
-console.log(`${accumulatedIncome > 0 ? `Ваши свободные средства на месяц с учетом расходов составляют: ${accumulatedIncome}` :
-  accumulatedIncome === 0 ? `Ваши доходы равны расходам. Средств для накопления не осталось` :
-  `Вы все истратили, копить нечего :) Вы еще и в минусе на ${accumulatedIncome} руб`}`);
-
-console.log(`Ваша цель - накопить ${purpose} рублей. ${targetMonth}`);
-
-console.log(`${budgetDay > 0 ? `Накоплено в день: ${budgetDay} руб` : 
-  budgetDay === 0 ? `Ежедневных накоплений нет` : 
-  `Вы каждый день уходите в минус на ${budgetDay} руб`}`);
+getData();
